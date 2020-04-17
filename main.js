f5db6d3dd180b453266046937dd49b14d0393048
Sean McPherson <git@seanmcp.com>
2020-04-17 16:35:32 -0400

    Remove preview image

--- main.js
+++ main.js
@@ -24,20 +24,16 @@ document.getElementById('xhr').addEventListener('click', async () => {
     }
 })
 
+const faviconLink = document.querySelector('link[rel="shortcut icon"]')
 const faviconReverseMap = {
     'favicon-slider.png': 'favicon-clap.png',
     'favicon-clap.png': 'favicon-slider.png',
 }
 
-const faviconLink = document.querySelector('link[rel="shortcut icon"]')
-const faviconPreview = document.getElementById('favicon-preview')
-
 document.getElementById('favicon').addEventListener('click', () => {
     const { href: oldHref } = faviconLink
     const oldFavicon = oldHref.slice(oldHref.lastIndexOf('/') + 1)
-    const newFavicon = faviconReverseMap[oldFavicon]
-    faviconLink.href = newFavicon
-    faviconPreview.src = oldFavicon
+    faviconLink.href = faviconReverseMap[oldFavicon]
 })
 
 const styleTag = document.querySelector('style')
