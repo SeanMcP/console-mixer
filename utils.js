76de4f158cddef73107bbb8087e3d02041f3206e
Sean McPherson <git@seanmcp.com>
2020-04-21 21:40:20 -0400

    Fix time format

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 export function getTime() {
     // Someone teach me to do this better
     const now = new Date()
-    return new Date().toLocaleTimeString().split(' ')[0] + `.${now.getMilliseconds()}`
+    return new Date().toLocaleTimeString([], { hour12: false }).split(' ')[0] + `.${String(now.getMilliseconds()).padStart(3, '0')}`
 }
